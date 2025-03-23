ea9e47d93ce04ba072783d989dfca28ee21c9410
Pettecco <petterson.ikaro@gec.inatel.br>
2025-03-23 09:07:33 -0300

    enable cors para o endereço do frontend

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors({
+		origin: 'http://localhost:5173',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type'],
+	});
+
 	const config = new DocumentBuilder()
 		.setTitle('PomoDone App')
 		.setVersion('1.0')
 		.build();
+
 	const documentFactory = () => SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('swagger', app, documentFactory);
 
